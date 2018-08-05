21ec7b2bd559c7c01162b4480ef33c7582317387
Vishal Sharma <vsh1819@gmail.com>
2018-08-05 05:01:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,63 @@
+function breakOut (array, changeValue, stopValue) {
+
+  for (var i = 0; i < array.length; i++) {
+
+    if (array[i] === stopValue) {
+
+        return array
+
+    }
+
+    array[i] = changeValue;
+  }
+
+
+
+}
+
+function keepGoing(array, changeValue, skipValue) {
+
+  for (var i = 0; i < array.length; i++) {
+
+    if (array[i] === skipValue) {
+
+        continue
+
+    }
+
+    array[i] = changeValue;
+  }
+
+
+  // array.forEach ( (element, index) => {
+  //   if (element === skipValue) {
+  //     continue
+  //   }
+  //
+  //   array[index] = changeValue
+  // })
+
+  return array
+
+
+}
+
+function findBy(array, findFn) {
+
+
+  for (var i = 0; i < array.length; i++) {
+
+    if (findFn(array[i])) {
+      return array[i]
+    }
+
+
+  }
+
+  return null
+
+
+
+
+
+}
