d0d02450b9f486b4b7b69f27ce9ca08b912e33f6
Vishal Sharma <vsh1819@gmail.com>
2018-08-05 04:44:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,49 @@
+function breakOut (array, changeValue, stopValue) {
+
+  for (var i = 0; i < array.length; i++) {
+
+    if (array[i] === stopValue) {
+
+        return array
+
+    }
+
+    array[i] = changeValue;
+  }
+
+
+
+}
+
+function keepGoing(array, changeValue, skipValue) {
+
+  for (var i = 0; i < array.length; i++) {
+
+    if (array[i] === skipValue) {
+
+        continue
+
+    }
+
+    array[i] = changeValue;
+  }
+
+
+  // array.forEach ( (element, index) => {
+  //   if (element === skipValue) {
+  //     continue
+  //   }
+  //
+  //   array[index] = changeValue
+  // })
+
+  return array
+
+
+}
+
+function findBy(array, findFn) {
+
+
+  
+}
