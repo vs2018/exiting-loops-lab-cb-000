7e8e291979b46e983c4ac29169380f70494b1f01
Vishal Sharma <vsh1819@gmail.com>
2018-08-05 05:00:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function findBy(array, findFn) {
 
   for (var i = 0; i < array.length; i++) {
 
-    findFn(i) {
+    findFn(array[i]) {
       if (typeof array[i] === Number)
       return array[i]
     }
